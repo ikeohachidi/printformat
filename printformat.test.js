44a7e22351b3a066c0254053affd8cbd9d79bd3d
Ikeoha Chidi <ikeohachidi@gmail.com>
2019-04-24 22:31:46 +0100

    shoot myself in the foot

--- printformat.test.js
+++ printformat.test.js
@@ -1,9 +1,9 @@
 // run some stupid tests here
 
 pft.print("hello world there you are %v", "john");
-pft.print("hello world there you are", "john");
-pft.print("hello world there %v you are %v baby", "ben", "john");
-pft.print("hello world there %v you are %b", 89);
-pft.print("hello world there you are %v", "john");
+//pft.print("hello world there you are", "john");
+pft.print("hello world there %v you my good friend are %b baby", "ben", 120);
+//pft.print("hello world there %v you are %b", 89);
+//pft.print("hello world there you are %v", "john");
 
-
+console.log(pft.getValue("hello there %v", "john"))
