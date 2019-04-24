02ebdab3cba59a7ede546ddcb254c6a4d9f13ab3
Ikeoha Chidi <ikeohachidi@gmail.com>
2019-04-24 23:43:53 +0100

    updated test

--- printformat.test.js
+++ printformat.test.js
@@ -1,9 +1,11 @@
 // run some stupid tests here
 
 pft.print("hello world there you are %v", "john");
-//pft.print("hello world there you are", "john");
+pft.print("hello world there you are", "john");
 pft.print("hello world there %v you my good friend are %b baby", "ben", 120);
-//pft.print("hello world there %v you are %b", 89);
-//pft.print("hello world there you are %v", "john");
+pft.print("hello world there %v you are %b", 89);
+pft.print("hello world there you are %v", "john");
 
-console.log(pft.getValue("hello there %v", "john"))
+pft.print("hello world there %t you %T my good friend are %o baby", "ben", {}, "hello");
+pft.print("hello world there %v you my good friend are %b baby", "ben", 120);
+pft.print("hello world there %v you my good friend are %x baby", "ben", 120);
